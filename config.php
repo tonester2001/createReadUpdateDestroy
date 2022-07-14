a52c1aed7ec1e40e944012ccb3ca3446e6726d4b
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-13 17:52:28 -0700

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
  * using mysqli_connect instead
  */
 
-$dbHost = 'your database host';
-$dbName = 'your database name';
-$dbUsername = 'your database username';
-$dbPassword = 'your database password';
+$dbHost = 'YOUR_DB_HOST';
+$dbName = 'YOUR_DB_NAME';
+$dbUsername = 'YOUR_DB_USERNAME';
+$dbPassword = 'YOUR_DB_PASSWORD';
 
 $mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
  
