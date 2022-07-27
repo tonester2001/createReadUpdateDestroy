d68ebd3a851b38174bf4f846322b6273581332f7
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-27 14:53:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,6 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 		echo "<td>".$res['state']."</td>";
 		echo "<td>".$res['country']."</td>";
 		echo "<td>".$res['about']."</td>";
-		echo "<td>".$res['profilePicture']."</td>";
 		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
 	}
 	?>
