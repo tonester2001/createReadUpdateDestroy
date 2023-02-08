a68e24437b4e644f6da246b85b67544dce05e3af
tonester2001 <114701040+tonester2001@users.noreply.github.com>
2023-02-07 21:02:55 -0800

    Update config.php

--- server/config.php
+++ server/config.php
@@ -8,7 +8,7 @@
 $dbHost = 'YOUR_DB_HOST';
 $dbName = 'YOUR_DB_NAME';
 $dbUsername = 'YOUR_DB_USERNAME';
-$dbPassword = 'YOUR_DB_PASSWORD');
+$dbPassword = 'YOUR_DB_PASSWORD';
 
 $mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
  
